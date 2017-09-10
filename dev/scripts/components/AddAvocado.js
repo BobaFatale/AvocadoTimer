c54bcf55e9c68b3a38be41f4f5648138e1928ef5
June Epstein <epstein.june@gmail.com>
2017-09-09 21:34:56 -0400

    form bug fixes

--- dev/scripts/components/AddAvocado.js
+++ dev/scripts/components/AddAvocado.js
@@ -6,12 +6,12 @@ class AddAvocado extends React.Component {
 			<div>
 				<form onSubmit={this.props.handleAdd}>
 					<label htmlFor="avocadoName">Give Your Avocado a Name</label>
-					<input type="text" id="avocadoName" name="avocadoName" placeholder="Give your avocado a name" value={this.props.avocadoName} onInput={this.props.handleInput} />
+					<input type="text" id="avocadoName" name="avocadoName" placeholder="Give your avocado a name" value={this.props.avocadoName} onChange={this.props.handleInput} />
 					<div className="addAvocado__range">
-						<input type="range" min='1' max='14' step='1' value={this.props.daysToRipe} name="daysToRipe" onInput={this.props.handleInput} />
+						<input type="range" min='1' max='14' step='1' value={this.props.daysToRipe} name="daysToRipe" onChange={this.props.handleInput} />
 					</div>
 					<label htmlFor="daysToRipe">Ripe in # of days:</label>
-					<input type="number" id="daysToRipe" name="daysToRipe" value={this.props.daysToRipe} min="1" max="14" onInput={this.props.handleInput}/>
+					<input type="number" id="daysToRipe" name="daysToRipe" value={this.props.daysToRipe} min="1" max="14" onChange={this.props.handleInput}/>
 					<div>
 						<label htmlFor="morningToD">Morning</label>
 						<input type="radio" name="timeOfDay" id="morningToD" value="0900" className="addAvocado__radioToD" onChange={this.props.handleInput} />
