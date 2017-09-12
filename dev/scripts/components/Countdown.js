01f2e9a09d1a45970a2971eaafbc7ab80d63e95d
June Epstein <epstein.june@gmail.com>
2017-09-11 22:13:46 -0400

    timer dynamically updates remaining time display

--- dev/scripts/components/Countdown.js
+++ dev/scripts/components/Countdown.js
@@ -3,16 +3,39 @@ import React from 'react';
 class Countdown extends React.Component {
 	constructor(){
 		super();
+		this.state = {
+			timeLeft: '',
+		}
+		this.runCountdown();
+		this.runCountdown = this.runCountdown.bind(this);
 	}
 	runCountdown(){
-		const currentTime = new Date();
-		const now = currentTime.getTime();
-		const timeLeft = (this.props.ripeDate - now);
-		let minutesLeft = Math.floor((timeLeft/1000/60) % 60);
-		let hoursLeft = Math.floor((timeLeft/(1000*60*60)) % 24);
-		let daysLeft = Math.floor(timeLeft/(1000*60*60*24));
-		const ripeDate = moment(this.props.ripeDate);
+		const parseTimeLeft = (ripeDate) => {
+			const currentTime = new Date();
+			const now = currentTime.getTime();
+			const timeLeft = (ripeDate - now);
+			let minutesLeft = Math.floor((timeLeft/1000/60) % 60);
+			let hoursLeft = Math.floor((timeLeft/(1000*60*60)) % 24);
+			let daysLeft = Math.floor(timeLeft/(1000*60*60*24));
+			return {
+				days: daysLeft,
+				hours: hoursLeft,
+				minutes: minutesLeft,
+			}
+		}
+		this.timerID = setInterval(() => this.tick(), 1000)
+		
+		this.tick = () => {
+			if(this.state.timeLeft.days <= 0 && this.state.timeLeft.hours <= 0) {
+				clearInterval(this.timerID)
+				this.timerEnds()
+			}
+			this.setState({
+				timeLeft: parseTimeLeft(this.props.ripeDate)
+			})
+		}
 	}
+
 	render(){
 		// const classes = className({
 		// 	'avocado__countdown':true,
@@ -22,10 +45,13 @@ class Countdown extends React.Component {
 			<div className='avocado__countdown'>
 				<div className="timer">
 					<div className="timer__days">
+					{this.state.timeLeft.days} Days
 					</div>
 					<div className="timer__hours">
+					{this.state.timeLeft.hours} Hours
 					</div>
 					<div className="timer__minutes">
+					{this.state.timeLeft.minutes} Minutes
 					</div>
 				</div>
 			</div>
