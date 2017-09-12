21d46b97a11e133bd0a1c61ddd60e95f85f18497
June Epstein <epstein.june@gmail.com>
2017-09-11 20:22:44 -0400

    connected server.js to firebase admin sdk added dependancies to package.json

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const app = express();
+const admin = require("firebase-admin");
+
+var serviceAccount = require("./avocadotimer-firebase-adminsdk-yhkom-7dba4dd1ef.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://avocadotimer.firebaseio.com"
+});
+
+const db = admin.database();
+const ref = db.ref("/App/pendingEmails/");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
 
 app.get("/",(req,res) => {
 	res.send('hello');
